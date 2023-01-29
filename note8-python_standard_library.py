d51370675076ad3def9edb110957164d1f4be204
Robin Correa <robin.correa21@gmail.com>
2023-01-29 12:10:19 +0800

    Working with JSON files in Python

--- note8-python_standard_library.py
+++ note8-python_standard_library.py
@@ -1,5 +1,8 @@
 # [Working with Paths]
 
+import time
+import json
+from datetime import datetime
 from pathlib import Path
 
 Path(r"C:\Program Files\Microsoft")  # prefix-r means raw string
@@ -21,7 +24,39 @@ print(path)  # ecommerce\file.test
 
 # [Working Directories]
 
-path = Path("ecommerce")
-path.mkdir()
-path.rmdir()
-path.rename("ecommerce2")
+# path = Path("ecommerce")
+# path.mkdir()
+# path.rmdir()
+# path.rename("ecommerce2")
+
+# [Working with DateTimes]
+
+dt1 = datetime(2018, 1, 1)
+print(dt1)  # 2018-01-01 00:00:00
+dt2 = datetime.now()
+print(dt2)  # 2023-01-29 11:53:03.203512
+# to tell python the specified date string to convert to object
+dt = datetime.strptime("2018/01/01", "%Y/%m/%d")
+print(dt)
+
+print(f"{dt.year}/{dt.month}")
+print(dt.strftime("%Y/%m"))
+
+print(dt2 > dt1)  # True
+
+print(time.time())  # total seconds since Jan 1970
+
+# [Working with JSON Files]
+
+movies = [
+    {"id": 1, "title": "Terminator", "year": 1984},
+    {"id": 3, "title": "Kindergarten Cop", "year": 1993},
+]
+
+# data = json.dumps(movies)
+# Path('movies.json').write_text(data)
+
+# Parse JSON from source
+data = Path('movies.json').read_text()
+movies = json.loads(data)
+print(movies[0])  # {'id': 1, 'title': 'Terminator', 'year': 1984}
