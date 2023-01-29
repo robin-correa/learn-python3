c70b7b26e4a66211832aa55feba3d6285aef06b5
Robin Correa <robin.correa21@gmail.com>
2023-01-29 11:18:26 +0800

    Learn Modules in Python

---
+++ note7-modules.py
@@ -0,0 +1,5 @@
+# [Creating Modules]
+# from note7modules.ecommerce.sales import calc_tax, calc_shipping
+from note7modules.ecommerce.shopping import sales
+
+sales.calc_tax()
---
+++ note7modules/ecommerce/shopping/sales.py
@@ -0,0 +1,12 @@
+#from ecommerce.customer import contact
+
+
+def calc_tax():
+    print("Calc Tax")
+
+
+def calc_shipping():
+    print("Calc Shipping")
+
+
+# contact.contact_customer()
